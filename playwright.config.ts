2c05ce9bd9d621e327b4e6fd420a72e7cbbbd8c9
Narayanan Kangath <Narayanan.Kangath@neudesic.com>
2025-05-05 11:33:59 +0530

    run ci on container

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
@@ -25,6 +25,7 @@ export default defineConfig({
       {
         name: "My Test Report",
         outputFile: "./monocart-report/index.html",
+        trend: './monocart-report/index.json'
       },
     ],
   ],
